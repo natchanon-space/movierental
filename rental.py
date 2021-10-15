60f580e27c5ed01a7e2d179acd27e07e160b7c29
natchanon-space <natchanon.space@gmail.com>
2021-10-15 23:51:29 +0700

    complete movierental

--- rental.py
+++ rental.py
@@ -1,6 +1,31 @@
 from movie import Movie
 import logging
 
+
+class PriceCode:
+	PRICE_CODE = {
+		Movie.REGULAR: {
+			"price": lambda days: 2.0 + 1.5*max(0, days-2),
+			"frp": lambda days: 1
+		},
+		Movie.CHILDRENS: {
+			"price": lambda days: 1.5 + 1.5*max(0, days-3),
+			"frp": lambda days: 1
+		},
+		Movie.NEW_RELEASE: {
+			"price": lambda days: 3.0*days,
+			"frp": lambda days: days
+		}
+	}
+
+	def __init__(self, movie):
+		self.movie_code = movie.get_price_code()
+
+	def price(self, days_rented):
+		if self.movie_code in self.PRICE_CODE.keys():
+			return self.PRICE_CODE[self.movie_code]["price"](days_rented)
+		raise KeyError()
+
 class Rental:
 	"""
 	A rental of a movie by customer.
@@ -19,6 +44,7 @@ class Rental:
 		"""
 		self.movie = movie
 		self.days_rented = days_rented
+		self.price_code = PriceCode(self.movie)
 
 	def get_movie(self):
 		return self.movie
@@ -28,20 +54,9 @@ class Rental:
 
 	def get_price(self):
 		amount = 0
-		if self.get_movie().get_price_code() == Movie.REGULAR:
-			# Two days for $2, additional days 1.50 each.
-			amount = 2.0
-			if self.get_days_rented() > 2:
-				amount += 1.5*(self.get_days_rented()-2)
-		elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-			# Three days for $1.50, additional days 1.50 each.
-			amount = 1.5
-			if self.get_days_rented() > 3:
-				amount += 1.5*(self.get_days_rented()-3)
-		elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-			# Straight per day charge
-			amount = 3*self.get_days_rented()
-		else:
+		try:
+			amount += self.price_code.price(self.days_rented)
+		except KeyError:
 			log = logging.getLogger()
 			log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
 		return amount
