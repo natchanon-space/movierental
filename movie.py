18925b4707f5d3d4929b32526e4f3396b09d96c4
natchanon-space <natchanon.space@gmail.com>
2021-11-06 01:09:22 +0700

    P2: add attributes to movie

--- movie.py
+++ movie.py
@@ -1,17 +1,28 @@
+from typing import List
+
+
 class Movie:
 	"""
 	A movie available for rent.
 	"""
-	def __init__(self, title: str):
+	def __init__(self, title: str, year: int, genre: List[str]):
 		# Initialize a new movie. 
-		self.title = title
+		self._title = title
+		self._year = year
+		self._genre = genre
+
+	def get_title(self):
+		return self._title
+
+	def get_year(self):
+		return self._year
+
+	def is_genre(self, genre: str):
+		return genre in self._genre
 
 	def get_price_code(self):
 		# get the price code
 		return self.price_code
-	
-	def get_title(self):
-		return self.title
 	
 	def __str__(self):
 		return self.title
